caf89b10288b3109d041ac7adc9a668739a69978
CarlosFrancoCifuentes <72622430+CarlosFrancoCifuentes@users.noreply.github.com>
2021-04-13 11:23:16 +0200

    build: configuración principal proyecto

---
+++ edu.pingpong.romanos/src/test/java/edu/pingpong/romanos/AppTest.java
@@ -0,0 +1,20 @@
+package edu.pingpong.romanos;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
